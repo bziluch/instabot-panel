3f1a1151f472a0ae5acf830a17afeca307c093ed
bziluch <bziluch@gmail.com>
2025-09-17 15:01:28 +0200

    feat: security + change password

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,20 +2,53 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserPasswordUpdateType;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 class UserController extends AbstractController
 {
-    #[Route(path: '/users', name: 'app_user_list', methods: ['GET'])]
+    #[Route(path: '/admin/users', name: 'app_user_list', methods: ['GET'])]
     public function index(
         UserRepository $repository,
     ): Response {
 
         return $this->render('user/index.html.twig', [
             'users' => $repository->findAll(),
+        ]);
+    }
+
+    #[Route(path: '/change-password', name: 'app_update_password', methods: ['GET', 'POST'])]
+    public function updatePassword(
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager,
+        Request $request,
+    ): Response {
+
+        /** @var User $user */
+        $form = $this->createForm(UserPasswordUpdateType::class, $user = $this->getUser());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $newPassword = $form->get('newPassword')->getData();
+
+            $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+            $user->setPassword($hashedPassword);
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Hasło zostało zmienione.');
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('user/change-password.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/UserPasswordUpdateType.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Validator\Constraints\EqualTo;
+use Symfony\Component\Validator\Constraints\IdenticalTo;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UserPasswordUpdateType extends AbstractType
+{
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {}
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('oldPassword', PasswordType::class, [
+                'mapped' => false,
+                'label' => 'Obecne hasło',
+                'constraints' => [new NotBlank()],
+            ])
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'first_options'  => ['label' => 'Nowe hasło'],
+                'second_options' => ['label' => 'Powtórz nowe hasło'],
+                'invalid_message' => 'Hasła nie zgadzają się',
+                'constraints' => [new NotBlank()],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Zmień hasło'
+            ])
+        ;
+
+        $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+
+            /** @var User $user */
+            $user = $event->getData();
+
+            $form = $event->getForm();
+            $oldPassword = $form->get('oldPassword')->getData();
+
+            if (!$this->passwordHasher->isPasswordValid($user, $oldPassword)) {
+                $form->get('oldPassword')->addError(new FormError('Nieprawidłowe hasło'));
+            }
+        });
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
